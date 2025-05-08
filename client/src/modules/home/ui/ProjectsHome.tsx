c09e62b5c4669cbcb7fbc79bed487f7b6b11fa27
reachAkash <akashgupta9062@gmail.com>
2025-05-08 14:15:46 +0530

    update : loader fixed

--- client/src/modules/home/ui/ProjectsHome.tsx
+++ client/src/modules/home/ui/ProjectsHome.tsx
@@ -1,19 +1,19 @@
 "use client";
 
-import { useProjectStore } from "@/states/store";
+import { useLoadingStore, useProjectStore } from "@/states/store";
 import { ProjectInterface } from "@/utils/types";
 import { ProjectCard } from "./ProjectCard";
 
 export const ProjectsHome = () => {
   const { projects } = useProjectStore();
-
+  const { loading } = useLoadingStore();
   return (
     <div className="w-full flex flex-col space-y-4">
       <div className="text-sm text-muted-foreground">Projects</div>
 
-      {projects.length === 0 ? (
+      {loading || projects.length === 0 ? (
         <div className="w-full h-[30vh] md:h-[50vh] flex items-center justify-center text-sm text-muted-foreground">
-          No Projects...
+          {loading ? "Loading..." : "No Projects..."}
         </div>
       ) : (
         <div className="w-full flex flex-wrap gap-4">
